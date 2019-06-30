721f882405c922576822770974808ed0b2eec69c
freemanwang <121689123@qq.com>
2019-06-30 14:49:39 +0800

    完成了框架搭建，以及首页模型

---
+++ src/components/tabbar/ShopcarContainer.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h3>ShopcarContainer</h3>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "HomeContainer"
+    }
+</script>
+
+<style scoped>
+
+</style>
